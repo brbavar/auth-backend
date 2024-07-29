9d584549c8818ede2c90f857de0aeabaef51db85
Ben Bavar <ben.bavar@gmail.com>
2024-07-28 20:18:13 -0400

    Update paths to validation files

--- api/index.js
+++ api/index.js
@@ -4,10 +4,10 @@ import express from 'express';
 import cors from 'cors';
 
 import { check } from 'express-validator';
-import { validateRegistration } from './validation/validateRegistration.js';
-import { validateNewPassword } from './validation/validateNewPassword.js';
-import { confirmMatch } from './validation/confirmMatch.js';
-import { getValidationErr } from './validation/getValidationErr.js';
+import { validateRegistration } from '../validation/validateRegistration.js';
+import { validateNewPassword } from '../validation/validateNewPassword.js';
+import { confirmMatch } from '../validation/confirmMatch.js';
+import { getValidationErr } from '../validation/getValidationErr.js';
 
 import {
   createAcct,
@@ -17,7 +17,7 @@ import {
   sendPasswordResetEmail,
   getPassword,
   resetPassword,
-} from './controllers/acct-controller.js';
+} from '../controllers/acct-controller.js';
 
 dotenv.config();
 
