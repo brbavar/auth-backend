2aaac941621911be93f531639d567a1bf5aec78b
Ben Bavar <ben.bavar@gmail.com>
2026-03-04 15:54:47 -0500

    Check if userData.rows[0] is defined rather than if userData.rows is truthy

--- controllers/acct-controller.js
+++ controllers/acct-controller.js
@@ -130,14 +130,14 @@ const logIn = async (req, res) => {
   const userData = await getUserData(req.params);
   console.log(`login accessed these user data:\n\nrows: ${userData.rows}`);
 
-  if (!userData.rows /* && resToGetUserData.status !== 304 */)
+  if (userData.rows[0] === undefined /* && resToGetUserData.status !== 304 */)
     return res.sendStatus(401);
 
   const passwordRight = await bcrypt.compare(
     req.params.Password,
-    userData.rows
-      ? userData.rows[0].password
-      : cache[req.params.Email].Password,
+    userData.rows[0] === undefined
+      ? cache[req.params.Email].Password
+      : userData.rows[0].password,
   );
 
   if (!passwordRight) return res.sendStatus(401);
